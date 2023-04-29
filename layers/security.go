da638ff6177814a416835c1a923e65a2122ee646
Divan Visagie <me@divanv.com>
2023-04-29 17:58:40 +0200

    Multi user

--- layers/security.go
+++ layers/security.go
@@ -1,35 +1,51 @@
 package layers
 
 import (
-	"fmt"
 	"os"
 	"ratatoskr/types"
+	"strings"
 )
 
 type Security struct {
 	//https://github.com/divanvisagie/Ratatoskr
-	admin string
+	users []string
 	child Layer
 }
 
 func NewSecurity(child Layer) *Security {
-	//get from env variable
+	//admin user
 	admin := os.Getenv("TELEGRAM_ADMIN")
+	//list of comma separated users
+	users := os.Getenv("TELEGRAM_USERS")
+
+	user_list := strings.Split(users, ",")
+
+	//add admin user to list of users
+	user_list = append(user_list, admin)
 
 	return &Security{
-		admin: admin,
 		child: child,
+		users: user_list,
 	}
 }
 
 func (s *Security) PassThrough(req *types.RequestMessage) (types.ResponseMessage, error) {
-	if req.UserName != s.admin {
+	//check if user is in list of users
+	authorized := false
+	for _, user := range s.users {
+		if user == req.UserName {
+			authorized = true
+			break
+		}
+	}
+
+	if !authorized {
 		return types.ResponseMessage{
-			Message: fmt.Sprintf(`
-				Only %s is authorized to use this bot.
+			Message: strings.TrimSpace(`
+				Only certain users are authorized to use this bot.
 				However the code is open source and you can run your own instance of the bot.
 				Just visit https://github.com/divanvisagie/Ratatoskr for more info. 
-			`, s.admin),
+			`),
 			ChatID: req.ChatID,
 		}, nil
 	}
