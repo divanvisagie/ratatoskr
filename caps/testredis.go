c5d8abdfcc6da3136eb1e152cbaa9dea426db7be
Divan Visagie <me@divanv.com>
2023-05-10 21:07:42 +0200

    Redis

---
+++ caps/testredis.go
@@ -0,0 +1,59 @@
+package caps
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"ratatoskr/types"
+	"strings"
+	"time"
+
+	"github.com/redis/go-redis/v9"
+)
+
+type TestRedis struct {
+}
+
+func NewTestRedis() *TestRedis {
+	return &TestRedis{}
+}
+
+func (c TestRedis) Check(req *types.RequestMessage) bool {
+	return strings.ToLower(req.Message) == "test redis"
+}
+
+func (c TestRedis) Execute(req *types.RequestMessage) (types.ResponseMessage, error) {
+	ctx := context.Background()
+
+	url := os.Getenv("REDIS_URL")
+	opt, err := redis.ParseURL(url)
+	if err != nil {
+		res := types.ResponseMessage{
+			ChatID:  req.ChatID,
+			Message: "Redis is not working",
+		}
+		return res, nil
+	}
+
+	rdb := redis.NewClient(opt)
+	now := time.Now()
+	timestamp := now.UnixMilli()
+	key := fmt.Sprintf("%d", timestamp)
+
+	rdb.Set(ctx, key, "working", time.Millisecond*500)
+
+	answer, err := rdb.Get(ctx, key).Result()
+	if err != nil {
+		res := types.ResponseMessage{
+			ChatID:  req.ChatID,
+			Message: "Redis broke on retreival",
+		}
+		return res, nil
+	}
+
+	res := types.ResponseMessage{
+		ChatID:  req.ChatID,
+		Message: fmt.Sprintf("Redis is working: %s", answer),
+	}
+	return res, nil
+}
--- go.mod
+++ go.mod
@@ -8,6 +8,7 @@ require (
 	github.com/gocolly/colly v1.2.0
 	github.com/sashabaranov/go-openai v1.9.0
 	github.com/tiktoken-go/tokenizer v0.1.0
+	github.com/redis/go-redis/v9 v9.0.4
 )
 
 require (
@@ -15,7 +16,9 @@ require (
 	github.com/antchfx/htmlquery v1.3.0 // indirect
 	github.com/antchfx/xmlquery v1.3.15 // indirect
 	github.com/antchfx/xpath v1.2.4 // indirect
+	github.com/cespare/xxhash/v2 v2.2.0 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
 	github.com/dlclark/regexp2 v1.9.0 // indirect
 	github.com/gobwas/glob v0.2.3 // indirect
 	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
