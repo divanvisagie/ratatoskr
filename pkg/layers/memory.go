7a3ed6102374bd3bace86ec111a01ef295ae039b
Divan visagie <me@dvisagie.com>
2023-12-29 12:58:56 +0100

    Ectract memory context creator

--- pkg/layers/memory.go
+++ pkg/layers/memory.go
@@ -18,19 +18,34 @@ func NewMemoryLayer(repo *repos.MessageRepo, child Layer) *MemoryLayer {
 	}
 }
 
+func (m *MemoryLayer) getContext(username string) ([]types.StoredMessage, error) {
+	// Get the context in order to feed future prompts
+	history, err := m.repo.GetMessages(username)
+	if err != nil {
+		fmt.Printf("Error in memory layer when getting messages: %v\n", err)
+		return nil, err
+	}
+
+	// Select only the latest 10 messages
+	if len(history) > 10 {
+		history = history[len(history)-10:]
+	}
+	fmt.Printf("History: %v\n", history)
+	return history, nil
+}
+
 func (m *MemoryLayer) PassThrough(req *types.RequestMessage) (types.ResponseMessage, error) {
 	inputMessage := repos.NewStoredMessage(repos.User, req.Message)
 	m.repo.SaveMessage(req.UserName, *inputMessage)
 
-	fmt.Printf("Saved inout message in memory layer: %v\n", req.Message)
-	history, err := m.repo.GetMessages(req.UserName)
+	history, err := m.getContext(req.UserName)
 	if err != nil {
-		fmt.Printf("Error in memory layer when getting messages: %v\n", err)
+		fmt.Printf("Error in memory layer when getting context: %v\n", err)
 		return types.ResponseMessage{}, err
 	}
 	req.Context = history
-	fmt.Printf("Memory layer got history: %+v\n", history)
 
+	// Now pass through child layer
 	res, err := m.child.PassThrough(req)
 	if err != nil {
 		fmt.Printf("Error in memory layer when passing through child layer: %v\n", err)
