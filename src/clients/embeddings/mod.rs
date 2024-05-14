cddfcc3b9f9b6b8ab92f9007e5f06254d974d150
Divan Visagie <me@divanv.com>
2024-05-14 21:50:53 +0200

    Refactor embeddings client
    Move implementations into their own files

---
+++ src/clients/embeddings/mod.rs
@@ -0,0 +1,60 @@
+use std::env;
+
+use async_trait::async_trait;
+use serde::{Deserialize, Serialize};
+use tracing::{error, info};
+
+pub use openai::OpenAiEmbeddingsClient;
+pub use ollama::OllamaEmbeddingsClient;
+
+pub mod openai;
+pub mod ollama;
+
+#[derive(Debug, Serialize, Deserialize)]
+struct EmbeddingsRequest {
+    input: String,
+    model: String,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct EmbeddingsResponse {
+    object: String,
+    data: Vec<Data>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct Data {
+    object: String,
+    index: usize,
+    embedding: Vec<f32>,
+}
+
+#[async_trait]
+pub trait EmbeddingsClient: Send + Sync {
+    async fn get_embeddings(
+        &self,
+        text: String,
+    ) -> Result<Vec<f32>, Box<dyn std::error::Error + Send + Sync>>;
+}
+
+/**
+ * Mocking the embeddings client
+ */
+pub struct MockEmbeddingsClient {}
+impl MockEmbeddingsClient {
+    #[allow(dead_code)]
+    pub fn new() -> Self {
+        MockEmbeddingsClient {}
+    }
+}
+
+#[async_trait]
+impl EmbeddingsClient for MockEmbeddingsClient {
+    async fn get_embeddings(
+        &self,
+        text: String,
+    ) -> Result<Vec<f32>, Box<dyn std::error::Error + Send + Sync>> {
+        info!("Mocking embeddings for: {}", text);
+        Ok(vec![0.0, 0.0, 0.0])
+    }
+}
---
+++ src/clients/embeddings/ollama.rs
@@ -0,0 +1,79 @@
+use std::env;
+
+use async_trait::async_trait;
+use reqwest::header;
+use serde::{Deserialize, Serialize};
+use tracing::{error, info};
+
+use super::{EmbeddingsClient, EmbeddingsRequest, EmbeddingsResponse};
+
+/// Ollama Client
+/// Implementation of the EmbeddingsClient trait which uses the Ollama service
+pub struct OllamaEmbeddingsClient<'a> {
+    base_url: &'a str,
+}
+
+/*
+* https://www.sbert.net/docs/pretrained_models.html
+* curl http://localhost:11434/api/embeddings -d '{
+*  "model": "all-minilm",
+*  "prompt": "Here is an article about llamas..."
+* }'
+**/
+
+#[derive(Serialize)]
+struct OllamaRequest {
+    model: String,
+    prompt: String,
+}
+
+#[derive(Deserialize)]
+struct OllamaResponse {
+    embedding: Vec<f32>,
+}
+
+impl OllamaEmbeddingsClient<'_> {
+    //ignore unused
+    #[allow(dead_code)]
+    pub fn new() -> Self {
+        OllamaEmbeddingsClient {
+            base_url: "http://localhost:11434",
+        }
+    }
+}
+
+#[async_trait]
+impl EmbeddingsClient for OllamaEmbeddingsClient<'_> {
+    async fn get_embeddings(
+        &self,
+        text: String,
+    ) -> Result<Vec<f32>, Box<dyn std::error::Error + Send + Sync>> {
+        info!("Ollama embeddings for: {}", text);
+        let url = format!("{}/api/embeddings", self.base_url,);
+        let client = reqwest::Client::new();
+
+        let request_body = serde_json::to_string(&OllamaRequest {
+            model: "all-minilm".to_string(),
+            prompt: text.to_string(),
+        });
+
+        let response = client.post(&url).body(request_body.unwrap()).send().await;
+
+        let ollama_response = match response {
+            Ok(response) => response.text().await.unwrap(),
+            Err(e) => {
+                error!("Error in response: {}", e);
+                return Err(Box::new(e));
+            }
+        };
+        let response_object: OllamaResponse = match serde_json::from_str(&ollama_response) {
+            Ok(object) => object,
+            Err(e) => {
+                error!("Error in respone object: {}", e);
+                return Err(Box::new(e));
+            }
+        };
+
+        Ok(response_object.embedding)
+    }
+}
