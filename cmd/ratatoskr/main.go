c729ff88264614cf87479f639b61e829147118e3
Divan Visagie <me@divanv.com>
2024-10-12 10:41:32 +0200

    Implement initial setup for default user in bot configuration
    In this commit, a function for the initial setup was added to the main.go file. This creates a User document with the 'Owner' username, which is read from the environment variable during config load. 'Owner' and 'BotUsername' parameters also added to the Config structure. The initial setup function runs in main, setting up a default user for the first time when the bot starts.

--- cmd/ratatoskr/main.go
+++ cmd/ratatoskr/main.go
@@ -2,11 +2,24 @@ package main
 
 import (
 	"github.com/divanvisagie/ratatoskr/internal/config"
+	"github.com/divanvisagie/ratatoskr/pkg/store"
 	"github.com/divanvisagie/ratatoskr/pkg/telegram"
 )
 
+func initialSetup(cfg *config.Config) {
+	s := store.NewDocumentStore()	
+
+	usr := store.User{
+		Username: cfg.Owner,
+	}
+
+	s.SaveUser(usr)
+}
+
 func main() {
 	cfg := config.LoadConfig()
+
+	initialSetup(cfg)
 
 	telegram.StartBot(cfg.TelegramToken, cfg)
 }
--- internal/config/config.go
+++ internal/config/config.go
@@ -5,11 +5,15 @@ import "os"
 type Config struct {
 	TelegramToken string
 	OpenAIKey     string
+	Owner         string
+	BotUsername   string
 }
 
 func LoadConfig() *Config {
 	return &Config{
 		TelegramToken: os.Getenv("TELEGRAM_BOT_TOKEN"),
 		OpenAIKey:     os.Getenv("OPENAI_API_KEY"),
+		Owner:         os.Getenv("RATATOSKR_OWNER"),
+		BotUsername:   os.Getenv("RATATOSKR_BOT_USERNAME"),
 	}
 }
